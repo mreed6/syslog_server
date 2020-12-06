371c8da012fdfe57b1be28a4f2c2d5b3012746ac
Camron Grant <camrongrant@oakland.edu>
2020-12-06 15:06:04 -0500

    deleted # of logs

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -98,25 +98,6 @@
           <div class="row">
             <div class="col-lg-3 col-md-6 col-sm-6">
               <div class="card card-stats">
-                <div class="card-header card-header-warning card-header-icon">
-                  <div class="card-icon">
-                    <i class="material-icons">content_copy</i>
-                  </div>
-                  <p class="card-category"># of Logs</p>
-                  <h3 class="card-title">1000
-                    <small>Logs</small>
-                  </h3>
-                </div>
-                <div class="card-footer">
-                  <div class="stats">
-                    <i class="material-icons text-danger">warning</i>
-                    <a href="javascript:;">Need more logs...</a>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col-lg-3 col-md-6 col-sm-6">
-              <div class="card card-stats">
                 <div class="card-header card-header-success card-header-icon">
                   <div class="card-icon">
                     <i class="material-icons">computer</i>
